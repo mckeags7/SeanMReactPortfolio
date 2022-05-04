109941ad1c56fe1ffe0a0b495a3223696e80b4ab
Sean <mckeags7@gmail.com>
2022-05-04 18:44:42 -0400

    fixed project order

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,7 +8,8 @@ function Project(props) {
       <div className="columns is-desktop is-justify-content-center is-flex-wrap-wrap is-flex-direction-row">
         {props.projects.map((project) => (
           <div className="column is-half">
-            <div className="card">
+            <div className="card" style={{'height':'500px!important'}}>
+
               <div className="card-image">
                 <figure className="image is-4by3">
                   <a href={project.live} target="_blank" rel="noreferrer">
