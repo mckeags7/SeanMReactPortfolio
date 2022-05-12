ed1a9d7a4206530afe2d88e977281a23a38658cd
Sean <mckeags7@gmail.com>
2022-05-11 20:19:10 -0400

    resume download fix

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,26 +1,62 @@
+/* eslint-disable react/jsx-no-undef */
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import pdf from "../../public/img/Sean Mckeag Resume - (May, 2022).pdf"
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+const resumeLink = "C:/Users/18608/Documents/GitHub/SeanMReactPortfolio/public/img/Sean Mckeag Resume - (May, 2022).pdf"
 
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
 
-import React from "react";
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
 
-function Resume() {
   return (
-    <div className="columns">
-      <div className="column">
-        <p className="content is-medium">Résumé</p>
-        <hr />
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
 
-        <a
-          className="button is-primary"
-          href={process.env.PUBLIC_URL + "../../build/img/Sean Mckeag Resume - (May, 2022).pdf"} download
-          src="../../public/img/Sean Mckeag Resume - (May, 2022).pdf"
-          target="_blank" rel="noreferrer"
-        >
-          <span className="icon">
-            <i className="fas fa-download"></i>
-          </span>
-          <span>Download My Resume</span>
-        </a>
-      </div>
+        <Row className="resume">
+          <Document file={resumeLink} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+
+
       <div className="column">
         <p className="content is-medium">Skills</p>
         <hr />
@@ -30,8 +66,5 @@ function Resume() {
             <li> Git</li>
         </ul>
       </div>
-    </div>
-  );
-}
 
-export default Resume;
+export default ResumeNew;
