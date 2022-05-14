76cdc8ef7aac7d03c3e0143d21633400b47fbe62
Sean <mckeags7@gmail.com>
2022-05-14 11:16:06 -0400

    update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,7 +9,7 @@ function Resume() {
 
         <a
           className="button is-primary"
-          href={process.env.PUBLIC_URL + "./Sean Mckeag Resume.pdf"}
+          href={process.env.PUBLIC_URL + "./Sean Mckeag Resume.pdf"} download
           target="_blank" rel="noreferrer"
         >
           <span className="icon">
